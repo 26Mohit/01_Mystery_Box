c4547780e505949572dcdc8caa7f075fba8d09e0
26Mohit <60586130+26Mohit@users.noreply.github.com>
2020-03-10 10:51:45 +1300

    Update 01_mystery_skeleton_v1.py.py

--- 01_mystery_skeleton_v1.py.py
+++ 01_mystery_skeleton_v1.py.py
@@ -24,10 +24,65 @@ class Start:
                                        command=lambda: self.to_game(1))
         self.lowstakes_button.grid(row=2, pady=10)
 
+    def to_game(self, stakes):
+        starting_balance = self.start_amount_entry.get()
+        Game(self, stakes, starting_balance)
+
+
+if __name__ == '__main__':
+    class Game:
+        def __init__(self, partner, stakes, starting_balance):
+            print(stakes)
+            print(starting_balance)
+
+            # disable low stakes button
+            partner.lowstakes_button.config(state=DISABLED)
+
+            # initialise variables
+            self.balance = IntVar()
+
+            # Set starting balance to amount entered by user at start of game
+            self.balance.set(starting_balance)
+
+            # GUI setup
+            self.game_box = Toplevel()
+            self.game_frame = Frame(self.game_box)
+            self.game_frame.grid()
+
+            # Heading row
+            self.heading_label = Label(self.game_frame, text="Heading",
+                                       font="Arial 24 bold", padx=10,
+                                       pady=10)
+            self.heading_label.grid(row=0)
+
+            # Balance Label
+            self.balance_frame = Frame(self.game_frame)
+            self.balance_frame.grid(row=1)
+
+            self.balance_label = Label(self.game_frame, text="Balance...")
+            self.balance_label.grid(row=2)
+
+            self.play_button = Button(self.game_frame, text="Gain",
+                                      padx=10, pady=10, command=self.reveal_boxes)
+            self.play_button.grid(row=3)
+
+        def reveal_boxes(self):
+            # reveal the balance from the initial function..
+            current_balance = self.balance.get()
+
+            # adjust the balance (subtract game cost and add pay out)
+            # For testing purposes, just add 2
+            current_balance +=2
+
+            # Set balance to adjust balance
+            self.balance.set(current_balance)
+
+            # Edit label so user can see their balance
+            self.balance_label.configure(text="Balance: {}".format(current_balance))
 
 # main routine
 if __name__ == "__main__":
     root = Tk()
-    root.title("title goes here")
-    something = Foo(root)
+    root.title("Mystery Box Game")
+    something = ()
     root.mainloop()
